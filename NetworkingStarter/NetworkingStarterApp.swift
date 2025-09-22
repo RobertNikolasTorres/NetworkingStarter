a5ac81a869542264c684ba5571fa91e44ecb7375
robert <robert.nikolas.torres@gmail.com>
2025-09-22 18:14:53 -0400

    Initial Commit

---
+++ NetworkingStarter/NetworkingStarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingStarterApp.swift
+//  NetworkingStarter
+//
+//  Created by Robert Nikolas Torres on 9/22/25.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingStarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
